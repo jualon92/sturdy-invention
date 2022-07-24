507276168a3d08e9827950ba46b8d17b7e1f4eb9
Juan Ignacio Londra <londrajuan@gmail.com>
2022-07-24 18:47:55 -0300

    Add spring boot security auth

---
+++ src/main/java/com/alkemy/disney/auth/entity/UserEntity.java
@@ -0,0 +1,79 @@
+package com.alkemy.disney.auth.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+
+@Entity
+@Table(name = "User")
+public class UserEntity implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Email
+    private String username;
+    @Size(min = 8)
+    private String password;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+
+    public void userEntity(){
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+        this.enabled = true;
+
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getId(){ return id;}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+}
---
+++ src/main/java/com/alkemy/disney/auth/service/UserDetailsCustomService.java
@@ -0,0 +1,41 @@
+package com.alkemy.disney.auth.service;
+
+import com.alkemy.disney.auth.dto.UserDTO;
+import com.alkemy.disney.auth.entity.UserEntity;
+import com.alkemy.disney.auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UserDetailsCustomService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException{
+
+        UserEntity userEntity = userRepository.findByUsername(userName);
+        if (userEntity == null){
+            throw new UsernameNotFoundException("Username or Password not found");
+        }
+        return new User(userEntity.getUsername(), userEntity.getPassword(), Collections.emptyList());
+    }
+
+    public boolean save(UserDTO userDTO){
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(userDTO.getUsername());
+        userEntity.setPassword(userDTO.getPassword());
+        userEntity = this.userRepository.save(userEntity);
+        if(userEntity !=null){
+            //emailService.sendWelcomeEmailTo(userEntity.getUsername());
+        }
+        return userEntity != null;
+    }
+}
